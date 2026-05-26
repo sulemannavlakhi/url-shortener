c656a1e805315b3ca3de6a168ab59ae3b2b99342
Your Name <you@example.com>
2026-05-26 02:28:44 -0400

    fixing syntax mistake from ecs to ecsv2

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "ecs_s3" {
+resource "aws_s3_bucket" "ecsv2_s3" {
   bucket = "sulemans3"
 }
 
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.ecd_s3.id
+  bucket = aws_s3_bucket.ecsv2_s3.id
 
   versioning_configuration {
     status = "Enabled"
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_state" {
-  bucket = aws_s3_bucket.ecs_s3.id
+  bucket = aws_s3_bucket.ecsv2_s3.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_public_access_block" "s3_state" {
 }
 
 resource "aws_ecr_repository" "ecsv2" {
-  name                 = "ecs-project"
+  name                 = "ecsv2-project"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
