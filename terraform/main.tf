623b3b602b1f0455f346a2e76a7b6a89c1713635
Your Name <you@example.com>
2026-06-04 18:41:08 -0400

    adding ecr module to root main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,11 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                  = var.azs
 }
+
+module "ecr" {
+  source = "./modules/ecr"
+
+  project_name = var.project_name
+  environment  = var.environment
+  services     = ["api", "dashboard", "worker"]
+}
