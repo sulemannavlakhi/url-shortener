21e0ab4c360dfe74e429f8d54cd748e64f1ce291
Your Name <you@example.com>
2026-06-05 23:32:52 -0400

    creating elasticache for caching

--- terraform/modules/redis/main.tf
+++ terraform/modules/redis/main.tf
@@ -3,3 +3,22 @@ resource "aws_elasticache_subnet_group" "main" {
   name       = "${var.project_name}-${var.environment}-redis-subnet-group"
   subnet_ids = var.private_subnet_ids
 }
+
+
+# creating elasticache using redis engine
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "${var.project_name}-${var.environment}-redis"
+  engine               = "redis"
+  engine_version       = "7.1"
+  node_type            = var.node_type
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [var.redis_sg_id]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-redis"
+  }
+}
