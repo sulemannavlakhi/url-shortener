2e68f90e2183cc0926243a666de0d6fd23c1f5e9
Your Name <you@example.com>
2026-06-05 23:27:36 -0400

    adding elasticache subnet group

---
+++ terraform/modules/redis/main.tf
@@ -0,0 +1,5 @@
+# elasticache placed in private subnet 
+resource "aws_elasticache_subnet_group" "main" {
+  name       = "${var.project_name}-${var.environment}-redis-subnet-group"
+  subnet_ids = var.private_subnet_ids
+}
