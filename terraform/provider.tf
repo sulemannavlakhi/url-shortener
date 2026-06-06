ff015ccfd19054dfc96f9cd20b048c6163cb2d18
Your Name <you@example.com>
2026-06-06 00:27:50 -0400

    adding root provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,3 +16,15 @@ terraform {
     encrypt      = true
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
