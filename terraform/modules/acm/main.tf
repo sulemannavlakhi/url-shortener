eaedcbc5d19e2f5ace43b5a6c6943394ceb9c3b6
Your Name <you@example.com>
2026-06-06 02:20:13 -0400

    creating cert resource for domain and subdomains

---
+++ terraform/modules/acm/main.tf
@@ -0,0 +1,10 @@
+# request cert for the domain and subdomains
+resource "aws_acm_certificate" "main" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
