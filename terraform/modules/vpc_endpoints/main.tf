d4526f21f45878a301888967452967c9f0d7de5f
Your Name <you@example.com>
2026-06-06 00:00:51 -0400

    adding vpc endpoint for cloudwatch

--- terraform/modules/vpc_endpoints/main.tf
+++ terraform/modules/vpc_endpoints/main.tf
@@ -37,3 +37,17 @@ resource "aws_vpc_endpoint" "docker" {
     Name = "${var.project_name}-${var.environment}-docker-endpoint"
   }
 }
+
+# vpc endpoint for cloudwatch logs
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = var.private_subnet_ids
+  security_group_ids  = [var.endpoint_sg_id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-logs-endpoint"
+  }
+}
