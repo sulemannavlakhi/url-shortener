c50e839f968ce9f4f947566de709a25888e19c1f
Your Name <you@example.com>
2026-06-05 23:51:01 -0400

    adding blue/green deployment for dashboard

--- terraform/modules/codedeploy/main.tf
+++ terraform/modules/codedeploy/main.tf
@@ -58,3 +58,53 @@ resource "aws_codedeploy_deployment_group" "api" {
     }
   }
 }
+
+# adding deployment group for dashboard 
+resource "aws_codedeploy_deployment_group" "dashboard" {
+  app_name               = aws_codedeploy_app.ecs.name
+  deployment_group_name  = "${var.project_name}-${var.environment}-dashboard-dg"
+  service_role_arn       = var.codedeploy_role_arn
+  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE", "DEPLOYMENT_STOP_ON_ALARM"]
+  }
+
+  ecs_service {
+    cluster_name = var.ecs_cluster_name
+    service_name = var.dashboard_service_name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [var.alb_listener_arn]
+      }
+
+      target_group {
+        name = var.dashboard_blue_target_group_name
+      }
+
+      target_group {
+        name = var.dashboard_green_target_group_name
+      }
+    }
+  }
+}
