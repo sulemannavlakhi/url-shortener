8e8915b53952790f60bcbbe19d2d80027be023be
Your Name <you@example.com>
2026-06-05 23:24:05 -0400

    creating db with sufficient resource paramaters

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -31,3 +31,28 @@ resource "aws_db_subnet_group" "main" {
     Name = "${var.project_name}-${var.environment}-db-subnet-group"
   }
 }
+
+# postgres db 
+resource "aws_db_instance" "postgres" {
+  identifier             = "${var.project_name}-${var.environment}-postgres"
+  engine                 = "postgres"
+  engine_version         = "16.3"
+  instance_class         = var.db_instance_class
+  allocated_storage      = 20
+  max_allocated_storage  = 100
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = random_password.db.result
+  port                   = 5432
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [var.rds_sg_id]
+  publicly_accessible    = false
+  storage_encrypted      = true
+  backup_retention_period = 7
+  deletion_protection    = false
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-postgres"
+  }
+}
