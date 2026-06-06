d3699359db7465ec68009b21f9efaf08d8ecd172
Your Name <you@example.com>
2026-06-05 22:53:09 -0400

    adding iam main.tf with execution role and trust policy for tasks

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "ecs_tasks_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "execution" {
+  name               = "${var.project_name}-${var.environment}-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "execution_managed" {
+  role       = aws_iam_role.execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
