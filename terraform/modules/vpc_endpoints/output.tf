55d5cecae224ef03f3d1471105f94e8c5d1ccaa7
Your Name <you@example.com>
2026-06-06 23:59:33 -0400

    replacing for each output

--- terraform/modules/vpc_endpoints/output.tf
+++ terraform/modules/vpc_endpoints/output.tf
@@ -1,7 +1,23 @@
-output "interface_endpoint_ids" {
-  value = { for k, v in aws_vpc_endpoint.interface : k => v.id }
-}
-
 output "s3_endpoint_id" {
   value = aws_vpc_endpoint.s3.id
+}
+
+output "ecr_endpoint_id" {
+  value = aws_vpc_endpoint.ecr.id
+}
+
+output "docker_endpoint_id" {
+  value = aws_vpc_endpoint.docker.id
+}
+
+output "logs_endpoint_id" {
+  value = aws_vpc_endpoint.logs.id
+}
+
+output "secretsmanager_endpoint_id" {
+  value = aws_vpc_endpoint.secretsmanager.id
+}
+
+output "sqs_endpoint_id" {
+  value = aws_vpc_endpoint.sqs.id
 }
